9202fc0ecea30f688ce52ea65700f6671a43e144
SantiPR00 <santiperez3@gmail.com>
2025-05-15 01:15:27 +0200

    Añadir repo GMM completo sin submódulos

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/santi/semantic/src
+# Build directory: /home/santi/semantic/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/santi/semantic/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/santi/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/santi/catkin_ws/devel;/opt/ros/noetic' != '' else []
